9a8638bc0383817efdb65f328a66969f8f37119f
Elie <elie@archireport.com>
2024-08-15 08:40:25 +0200

    feat: dynamic location and better style

--- src/components/cards/location.tsx
+++ src/components/cards/location.tsx
@@ -1,28 +1,100 @@
-import { Node } from "@xyflow/react";
-import React from "react";
+import { Node, useReactFlow } from "@xyflow/react";
+import React, { useState, useRef, useEffect } from "react";
 import BaseCard from "./baseCard";
 import CardHeader from "../ui/cardHeader";
-import backgroundImage from "/location.webp";
+import { cn } from "@/lib/utils";
 
 type LocationCardProps = {
   title: string;
+  description?: string;
+  image?: string;
 };
 
 const LocationCard: React.FC<Node<LocationCardProps>> = (props) => {
+  const { id, data } = props;
+  const { title, description, image: backgroundImage } = data;
+  const { updateNode } = useReactFlow();
+  const titleRef = useRef<HTMLTextAreaElement>(null);
+  const descriptionRef = useRef<HTMLTextAreaElement>(null);
+  const [image, setImage] = useState<string>(
+    backgroundImage || "/location.webp"
+  );
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (ev) => {
+        setImage(ev.target?.result as string);
+        updateNode(id, {
+          data: { ...data, image: ev.target?.result as string },
+        });
+      };
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => titleRef.current?.focus(), 10);
+  }, []);
+
+  useEffect(() => {
+    if (titleRef.current) {
+      titleRef.current.style.height = "auto";
+      titleRef.current.style.height = `${titleRef.current.scrollHeight}px`;
+    }
+  }, [title]);
+
+  useEffect(() => {
+    if (descriptionRef.current) {
+      descriptionRef.current.style.height = "auto";
+      descriptionRef.current.style.height = `${descriptionRef.current.scrollHeight}px`;
+    }
+  }, [description]);
+
   return (
     <BaseCard {...props}>
-      <div className="w-80 flex flex-col bg-background rounded overflow-hidden">
+      <div className="w-56 flex flex-col bg-background rounded overflow-hidden">
         <CardHeader color="bg-green-100" />
         <div className="bg-green-100/20 flex-grow">
-          <div className="flex gap-2 p-2 h-full">
-            <div className="relative h-48 w-full rounded-lg background-cover bg-cover p-6 flex flex-col justify-between">
+          <div className="w-full rounded-lg background-cover bg-cover p-2 flex flex-col gap-2 justify-between">
+            <div className="relative">
               <img
-                src={backgroundImage}
-                className="absolute top-0 left-0 w-full h-full object-cover rounded-lg border-2 border-white opacity-30"
+                src={image}
+                className={cn(
+                  "w-full h-full object-cover rounded-lg border-2 border-white shadow",
+                  backgroundImage ? "opacity-90" : "opacity-30"
+                )}
               />
-              <div className="relative text-3xl font-bold">Test</div>
-              <div className="relative w-48 h-48">carte</div>
+              <input
+                type="file"
+                accept="image/*"
+                className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+                onChange={handleImageChange}
+              />
             </div>
+            <textarea
+              ref={titleRef}
+              value={title}
+              onChange={(e) =>
+                updateNode(id, { data: { ...data, title: e.target.value } })
+              }
+              className={cn(
+                "bg-transparent p-1 text-2xl font-bold border-0 focus:outline-none resize-none w-full overflow-hidden",
+                !title.length && "border-b border-accent-foreground"
+              )}
+              rows={1}
+            />
+            <textarea
+              ref={descriptionRef}
+              value={description || ""}
+              onChange={(e) =>
+                updateNode(id, {
+                  data: { ...data, description: e.target.value },
+                })
+              }
+              className="bg-transparent p-1 w-full h-full flex-grow border-0 focus:outline-none resize-none text-xs"
+              rows={1}
+            />
           </div>
         </div>
       </div>
